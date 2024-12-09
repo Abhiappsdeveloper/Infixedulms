89016c22858feb93fea829924d23221f28958f5e
DIGITYX <digismartskills12@gmail.com>
2024-12-09 16:17:59 +0530

    iiii

--- config/database.php
+++ config/database.php
@@ -41,22 +41,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('https://auth-db1149.hstgr.io/'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'u709483251_infixedulms'),
-            'username' => env('DB_USERNAME', 'u709483251_infixedulms'),
-            'password' => env('DB_PASSWORD', 'Madhu@6291144591'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            // 'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'mysql2' => [
