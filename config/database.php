55372993940ae8811c6cd77a22b206d3dfcdac11
DIGITYX <digismartskills12@gmail.com>
2024-12-09 16:09:02 +0530

    iii

--- config/database.php
+++ config/database.php
@@ -47,13 +47,16 @@ return [
             'database' => env('DB_DATABASE', 'u709483251_infixedulms'),
             'username' => env('DB_USERNAME', 'u709483251_infixedulms'),
             'password' => env('DB_PASSWORD', 'Madhu@6291144591'),
-            // 'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => '',
-            'strict' => false,
+            'prefix_indexes' => true,
+            'strict' => true,
             'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mysql2' => [
