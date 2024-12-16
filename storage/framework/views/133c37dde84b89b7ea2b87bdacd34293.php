bb2984cd2680b903c026dc9b0d589ff3eadfd010
DIGITYX <digismartskills12@gmail.com>
2024-12-16 11:50:07 +0530

    Revert "iiiii"
    This reverts commit df6ef01f295e10d0d8c735ac53a5804ddb4640dc.

--- storage/framework/views/133c37dde84b89b7ea2b87bdacd34293.php
+++
@@ -1,3 +0,0 @@
-<?php echo $body; ?>
-
-<?php /**PATH /home/u709483251/domains/infixedulms.madhutechnoworld.in/public_html/resources/views/partials/email.blade.php ENDPATH**/ ?>
--- storage/framework/views/3402e24565b1e144aa35083609fb41b9.php
+++
@@ -1,8 +0,0 @@
-<div class="tab-pane fade" id="api_tab">
-    <div class="row">
-        <div class="col-12">
-            <?php echo $__env->make(theme('profile.api._zoom'), \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-        </div>
-    </div>
-</div>
-<?php /**PATH /home/u709483251/domains/infixedulms.madhutechnoworld.in/public_html/resources/views/frontend/infixlmstheme/profile/_api_tab.blade.php ENDPATH**/ ?>
